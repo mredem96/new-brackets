254c1d553827ae54c7f6e7c1b43e87c92252d03b
Hojiakbar Karimov <hojiakbar.karimov@periplus.ch>
2019-07-06 14:35:32 +0500

    brackets solution implemention of stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // // your solution
+    var stack = new Stack();
+    for (var currentBracket in str) {
+		var prevBracket = stack.getLastElement();
+		if (isCouple(prevBracket, currentBracket, bracketsConfig))
+			stack.pop();
+        else
+            stack.push(currentBracket);
+    }
+
+    return stack.length() === 0;
 }
